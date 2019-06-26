4aa53f301bacca7bf44bb24cc26817cf08bae2f4
uberto <uberto.gama@gmail.com>
2019-06-26 09:21:13 +0100

    solved

--- src/main/kotlin/com/ubertob/funkotlin/jsonCompactor/JsonCompactor.kt
+++ src/main/kotlin/com/ubertob/funkotlin/jsonCompactor/JsonCompactor.kt
@@ -2,9 +2,33 @@ package com.ubertob.funkotlin.jsonCompactor
 
 
 fun compactJson(json: Sequence<Char>): String =
-   json.fold("", ::compactor)
+   json.fold(OutQuotes(""), ::compactor).done
 
 
 
-fun compactor(done: String, c: Char): String =
-    if (c == ' ') done else done+c
+fun compactor(prev: JsonCompactor, c: Char): JsonCompactor =
+    prev.compact(c)
+
+
+sealed class JsonCompactor{
+    abstract val done: String
+    abstract fun compact(c: Char): JsonCompactor
+}
+
+data class InQuotes(override val done: String): JsonCompactor() {
+    override fun compact(c: Char): JsonCompactor =
+        if (c == '"')
+            OutQuotes(done+c)
+        else
+            InQuotes(done+c)
+}
+
+data class OutQuotes(override val done: String): JsonCompactor() {
+    override fun compact( c: Char): JsonCompactor =
+        if ( c in arrayOf(' ', '\n', '\t'))
+            this
+        else if (c == '"')
+            InQuotes(done+c)
+        else
+            OutQuotes(done+c)
+}
