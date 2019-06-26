ace6f499cfbd2abd1596d95abcea3ea476e59107
uberto <uberto.gama@gmail.com>
2019-06-26 09:16:37 +0100

    added failing tests

--- src/test/kotlin/com/ubertob/funkotlin/jsonCompactor/JsonCompactorTest.kt
+++ src/test/kotlin/com/ubertob/funkotlin/jsonCompactor/JsonCompactorTest.kt
@@ -17,4 +17,28 @@ class JsonCompactorTest {
 
     }
 
+
+    @Test
+    fun `strip the new lines and tabs`(){
+
+        val jsonText = "{ \t\t\"a\" : 20, \n \"b\": true }".asSequence()
+
+        val expected = "{\"a\":20,\"b\":true}"
+
+        assertThat( compactJson( jsonText )).isEqualTo(expected)
+
+    }
+
+
+    @Test
+    fun `keep spaces in quotes`(){
+
+        val jsonText = "{ \"my greetings\" :   \"hello world\" }".asSequence()
+
+        val expected = "{\"my greetings\":\"hello world\"}"
+
+        assertThat( compactJson( jsonText )).isEqualTo(expected)
+
+    }
+
 }
